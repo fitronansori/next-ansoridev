cfea635eff0750bdafecdbeecaef32009a27fdf4
fitronansori <fitronansori7@gmail.com>
2024-11-26 22:39:22 +0800

    Add navigation links constants

---
+++ src/constants/data.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    name: "Profile",
+    href: "/profile",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Certificates",
+    href: "/certificates",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
