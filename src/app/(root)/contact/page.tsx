55c86fdb7f9bfb8fa2c3a76f80a2c4bfa747bbbb
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:54:19 +0800

    remove ContactPage component

--- src/app/(root)/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-const ContactPage = () => {
-  return (
-    <section className="py-5 lg:py-10">
-      <div className="container">Contact</div>
-    </section>
-  );
-};
-export default ContactPage;
