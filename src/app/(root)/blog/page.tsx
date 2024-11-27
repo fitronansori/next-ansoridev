e16a29a35b6849ef14e57ffe76ed721572a1ecbd
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:55:51 +0800

    update Blog and Templates pages with development notice heading

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -1,7 +1,11 @@
 const BlogPage = () => {
   return (
     <section className="py-5 lg:py-10">
-      <div className="container">Blog</div>
+      <div className="container">
+        <h1 className="text-2xl font-bold">
+          Masih Dalam Tahap Pengembangan, Nantikan!
+        </h1>
+      </div>
     </section>
   );
 };
