846e5090ac731db94fbace4817c309a2acece60b
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:56:50 +0800

    replace CV download button with ButtonCV component in HomeLayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,19 +1,11 @@
+import ButtonCV from "@/components/common/ButtonCV";
 import Header from "@/components/layouts/Header";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header>
-        <Button className="hidden lg:flex" asChild>
-          <Link
-            href="/CV Muh. Fitron Ansori.pdf"
-            download="CV_Fitron Ansori"
-            target="_blank">
-            Download CV
-          </Link>
-        </Button>
+        <ButtonCV />
       </Header>
       {children}
     </div>
