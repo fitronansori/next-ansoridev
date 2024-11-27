f5be6b29aaaf2b34f1d2d60089586f5fd2e9501d
fitronansori <fitronansori7@gmail.com>
2024-11-27 12:01:40 +0800

    Add CV download button to HomeLayout component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,20 @@
 import Header from "@/components/layouts/Header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Header />
+      <Header>
+        <Button className="hidden lg:flex" asChild>
+          <Link
+            href="/CV Muh. Fitron Ansori.pdf"
+            download="CV_Fitron Ansori"
+            target="_blank">
+            Download CV
+          </Link>
+        </Button>
+      </Header>
       {children}
     </div>
   );
