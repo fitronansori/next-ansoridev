52c7c95ac384510a9e022e90b3498bf61cad540d
fitronansori <fitronansori7@gmail.com>
2024-11-27 21:08:57 +0800

    enhance ButtonCV component to accept custom className prop

--- src/components/common/ButtonCV.tsx
+++ src/components/common/ButtonCV.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 type ButtonCVType = {
   size?: "default" | "sm" | "lg";
+  className?: string;
 };
 
-const ButtonCV = ({ size = "default" }: ButtonCVType) => {
+const ButtonCV = ({ size = "default", className }: ButtonCVType) => {
   return (
-    <Button size={size} asChild>
+    <Button size={size} className={cn("", className)} asChild>
       <Link
         href="/CV_FitronAnsori.pdf"
         download="CV_Fitron Ansori"
