9f96ef5a0ef776bcfa0295289ee3adf11145da66
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:54:39 +0800

    add ButtonWA component for WhatsApp messaging

---
+++ src/components/common/ButtonWA.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { RiWhatsappFill } from "react-icons/ri";
+
+type ButtonWAProps = {
+  className?: string;
+  icon?: boolean;
+  variant?: "default" | "outline";
+  size?: "default" | "sm" | "lg";
+  children: React.ReactNode;
+};
+
+const ButtonWA = ({
+  children,
+  className,
+  icon = false,
+  variant = "default",
+  size = "default",
+}: ButtonWAProps) => {
+  return (
+    <Button size={size} variant={variant} className={cn("", className)} asChild>
+      <Link
+        href="https://api.whatsapp.com/send?phone=085738224566&text=Halo%20Fitron%20Ansori"
+        target="_blank"
+        rel="noopener noreferrer">
+        {children}{" "}
+        <RiWhatsappFill className={cn("", icon ? "block" : "hidden")} />
+      </Link>
+    </Button>
+  );
+};
+export default ButtonWA;
