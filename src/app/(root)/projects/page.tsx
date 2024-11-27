530bb37658583edf0bb98d90ed787d9bb9a3726a
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:56:25 +0800

    add Projects page component

--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -1,7 +1,25 @@
+import Title from "@/components/common/Title";
+import CardProject from "@/components/layouts/card/CardProject";
+import { projectData } from "@/constants/data";
+
 const ProjectsPage = () => {
   return (
     <section className="py-5 lg:py-10">
-      <div className="container">Projects</div>
+      <div className="container space-y-4">
+        <Title title="Projects" desc="Lihat project yang telah saya kerjakan" />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center lg:place-items-start gap-4 lg:gap-x-0 lg:gap-y-8">
+          {projectData.map((item) => (
+            <CardProject
+              key={item.id}
+              title={item.title}
+              github={item.github}
+              website={item.website}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
