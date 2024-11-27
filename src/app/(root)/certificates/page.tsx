6c2becf44124c4b1a55e5185295702bf4d6997d4
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:56:39 +0800

    add Certificates page

--- src/app/(root)/certificates/page.tsx
+++ src/app/(root)/certificates/page.tsx
@@ -1,8 +1,28 @@
-const CerificatesPage = () => {
+import Title from "@/components/common/Title";
+import CardCertificates from "@/components/layouts/card/CardCertificate";
+import { certificatesData } from "@/constants/data";
+
+const CertificatesPage = () => {
   return (
     <section className="py-5 lg:py-10">
-      <div className="container">Certificates</div>
+      <div className="container space-y-4">
+        <Title
+          title="Certificates"
+          desc="Lihat sertifikat yang telah saya dapatkan"
+        />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center lg:place-items-start gap-4 lg:gap-x-0 lg:gap-y-8">
+          {certificatesData.map((item) => (
+            <CardCertificates
+              key={item.id}
+              title={item.title}
+              link={item.link}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
-export default CerificatesPage;
+export default CertificatesPage;
