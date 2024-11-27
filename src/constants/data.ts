4d05d9917605015d4d5b6751750f1c1ddaeb898d
fitronansori <fitronansori7@gmail.com>
2024-11-27 20:54:27 +0800

    update navigation and add project, certificate, and service data

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -22,8 +22,8 @@ export const navLinks = [
     href: "/templates",
   },
   {
-    name: "Contact",
-    href: "/contact",
+    name: "Services",
+    href: "/services",
   },
   {
     name: "Login",
@@ -125,5 +125,86 @@ export const socialData: SocialType = [
     name: "WhatsApp",
     url: "https://api.whatsapp.com/send?phone=085738224566&text=Halo%20Fitron%20Ansori",
     icon: RiWhatsappFill,
+  },
+];
+
+export const projectData = [
+  {
+    id: 1,
+    title: "Suxz Platform",
+    github: "https://github.com/fitrondev/next-suxz",
+    website: "https://next-suxz.vercel.app/",
+    image: "/assets/images/p1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Zone Tech",
+    github: "https://github.com/fitrondev/react-zonetech",
+    website: "https://react-zonetech.vercel.app/",
+    image: "/assets/images/p2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Bookmark Landing Page",
+    github: "https://github.com/fitrondev/bookmark-landing-page",
+    website: "https://bookmark-landing-page-neon.vercel.app/",
+    image: "/assets/images/p3.png",
+  },
+  {
+    id: 4,
+    title: "Easy Bank Landing Page",
+    github: "https://github.com/fitrondev/easybank-landing-page",
+    website: "https://easybank-landing-page-xi-bay.vercel.app/",
+    image: "/assets/images/p4.png",
+  },
+];
+
+export const certificatesData = [
+  {
+    id: 1,
+    title:
+      "HTML, CSS dan JavaScript : Pemula sampai Mahir - Programmer Zaman Now",
+    link: "https://www.udemy.com/certificate/UC-e4f366c3-a76f-4e9e-915c-6d54e61fc2ea/",
+    image: "/assets/images/c3.jpg",
+  },
+  {
+    id: 2,
+    title: "FRONT-END WEB DEVELOPMENT - MSIB CYCLE 3",
+    link: "https://drive.google.com/file/d/1KfE2ddax-GrisYa8xJqEuNhB8ReVioLh/view",
+    image: "/assets/images/c1.png",
+  },
+  {
+    id: 3,
+    title:
+      "TOP 9 DEMO SKILVUL #TECH4IMPACT: FRONT-END & BACK-END WEB DEVELOPMENT",
+    link: "https://drive.google.com/file/d/1Qh6zP382PsHDUADAXhjfoN4eFWc5pokg/view",
+    image: "/assets/images/c2.png",
+  },
+  {
+    id: 4,
+    title: "Data Science MSIB Batch 5 Rakamin Academy",
+    link: "https://drive.google.com/file/d/1jjAXnZk_BUQy4vbXRDEjjUSUgvSpnlZe/view?usp=drive_link",
+    image: "/assets/images/c4.png",
+  },
+];
+
+export const servicesData = [
+  {
+    id: 1,
+    title: "Web Development",
+    desc: "Membuat website yang sesuai dengan kebutuhan Anda. Mulai dari website company profile, e-commerce, blog, dan lainnya.",
+    image: "/assets/images/s1.jpg",
+  },
+  {
+    id: 2,
+    title: "Web Design",
+    desc: "Mendesain website yang menarik dan responsif. Dengan desain yang menarik, website Anda akan lebih menarik.",
+    image: "/assets/images/s2.jpg",
+  },
+  {
+    id: 3,
+    title: "SEO Optimization",
+    desc: "Meningkatkan peringkat website Anda di mesin pencari. Dengan SEO, website Anda akan lebih mudah ditemukan.",
+    image: "/assets/images/s3.jpg",
   },
 ];
