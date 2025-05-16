6ba929af9b2884c885eedecb461b3f723fb8dd3f
fitronansori <fitronansori7@gmail.com>
2025-05-16 10:48:23 +0800

    feat: enhance CardCertificates component with additional props and improved layout

--- src/app/(root)/certificates/page.tsx
+++ src/app/(root)/certificates/page.tsx
@@ -5,19 +5,23 @@ import { certificatesData } from "@/constants/data";
 const CertificatesPage = () => {
   return (
     <section className="py-5 lg:py-10">
-      <div className="container space-y-4">
+      <div className="container space-y-5 md:space-y-10">
         <Title
           title="Certificates"
           desc="Lihat sertifikat yang telah saya dapatkan"
         />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center lg:place-items-start gap-4 lg:gap-x-0 lg:gap-y-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {certificatesData.map((item) => (
             <CardCertificates
               key={item.id}
               title={item.title}
               link={item.link}
               image={item.image}
+              desc={item.desc}
+              badge={item.badge}
+              awardBy={item.awardBy}
+              date={item.date}
             />
           ))}
         </div>
--- src/components/layouts/card/CardCertificate.tsx
+++ src/components/layouts/card/CardCertificate.tsx
@@ -1,43 +1,75 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Award, Calendar, ExternalLink } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { TbFileCertificate } from "react-icons/tb";
 
 type CardCertificateType = {
   title: string;
+  desc: string;
   link: string;
   image: string;
+  badge: string;
+  awardBy: string;
+  date: string;
 };
 
-const CardCertificates = ({ title, link, image }: CardCertificateType) => {
+const CardCertificates = ({
+  title,
+  desc,
+  link,
+  image,
+  badge,
+  awardBy,
+  date,
+}: CardCertificateType) => {
   return (
-    <Card className="max-w-sm shadow-md">
-      <CardHeader className="p-4">
+    <Card className="overflow-hidden hover:shadow-lg transition-all duration-300 pt-0 pb-4 gap-4">
+      <div className="relative h-52 bg-gray-100">
         <Image
-          src={image}
-          alt=""
-          width={1000}
-          height={300}
-          sizes="100%"
-          loading="lazy"
-          className="w-full h-[200px] object-fill rounded-md dark:brightness-90"
+          src={image || "/assets/images/placeholder.webp"}
+          alt="Sertifikat AWS"
+          fill
+          className="size-full object-cover"
         />
+
+        <div className="absolute top-3 right-3">
+          <Badge className="bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white">
+            {badge}
+          </Badge>
+        </div>
+      </div>
+
+      <CardHeader className="px-4">
+        <div className="flex items-center gap-2 mb-1">
+          <Award className="h-4 w-4" />
+          <CardDescription>{awardBy}</CardDescription>
+        </div>
+
+        <CardTitle className="text-lg line-clamp-2">{title}</CardTitle>
       </CardHeader>
 
-      <CardContent className="px-4 pb-4">
-        <CardTitle className="text-lg line-clamp-2">{title}</CardTitle>
+      <CardContent className="px-4">
+        <div className="flex items-center text-sm text-gray-500 mb-2">
+          <Calendar className="h-4 w-4 mr-1" />
+          <span>Diperoleh: {date}</span>
+        </div>
+
+        <p className="text-sm text-muted-foreground line-clamp-3">{desc}</p>
       </CardContent>
-      <CardFooter className="px-4 pb-4 justify-between">
-        <Button asChild>
-          <Link href={link} target="_blank" rel="noopener noreferrer">
-            <TbFileCertificate /> Link Certificate
+
+      <CardFooter className="px-4">
+        <Button size={"sm"} asChild>
+          <Link href={link} target="_blank">
+            <ExternalLink className="size-5" /> Verifikasi
           </Link>
         </Button>
       </CardFooter>
