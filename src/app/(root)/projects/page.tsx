abbfa5394f3d6f9b60aa7e212444d864eee062a5
fitronansori <fitronansori7@gmail.com>
2025-05-16 10:48:32 +0800

    feat: enhance ProjectsPage and CardProject component with additional props and improved layout

--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -5,19 +5,25 @@ import { projectData } from "@/constants/data";
 const ProjectsPage = () => {
   return (
     <section className="py-5 lg:py-10">
-      <div className="container space-y-4">
+      <div className="container space-y-5 md:space-y-10">
         <Title title="Projects" desc="Lihat project yang telah saya kerjakan" />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center lg:place-items-start gap-4 lg:gap-x-0 lg:gap-y-8">
-          {projectData.map((item) => (
-            <CardProject
-              key={item.id}
-              title={item.title}
-              github={item.github}
-              website={item.website}
-              image={item.image}
-            />
-          ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projectData
+            .slice()
+            .sort((a, b) => b.id - a.id)
+            .map((item) => (
+              <CardProject
+                key={item.id}
+                title={item.title}
+                github={item.github}
+                website={item.website}
+                image={item.image}
+                desc={item.desc}
+                tools={item.tools}
+                badge={item.badge}
+              />
+            ))}
         </div>
       </div>
     </section>
--- src/components/layouts/card/CardProject.tsx
+++ src/components/layouts/card/CardProject.tsx
@@ -1,7 +1,9 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -12,39 +14,66 @@ import { RiChromeFill, RiGithubFill } from "react-icons/ri";
 
 type CardProjectType = {
   title: string;
+  desc: string;
+  tools: string[];
+  badge: string;
   github: string;
   website: string;
   image: string;
 };
 
-const CardProject = ({ title, github, website, image }: CardProjectType) => {
+const CardProject = ({
+  title,
+  desc,
+  tools,
+  badge,
+  github,
+  website,
+  image,
+}: CardProjectType) => {
   return (
-    <Card className="max-w-sm shadow-md">
-      <CardHeader className="p-4">
+    <Card className="overflow-hidden hover:shadow-lg transition-all duration-300 flex flex-col pt-0 pb-4 gap-4">
+      <div className="relative h-52 bg-gray-100">
         <Image
-          src={image}
-          alt=""
-          width={1000}
-          height={300}
-          sizes="100%"
-          loading="lazy"
-          className="w-full h-[200px] object-fill rounded-md dark:brightness-90"
+          src={image || "/assets/images/placeholder.webp"}
+          alt="E-Commerce Website"
+          fill
+          className="size-full object-fill"
         />
+
+        <div className="absolute top-3 right-3">
+          <Badge className="bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white">
+            {badge}
+          </Badge>
+        </div>
+      </div>
+
+      <CardHeader className="px-4">
+        <CardTitle className="text-xl">{title}</CardTitle>
+        <CardDescription>{desc}</CardDescription>
       </CardHeader>
 
-      <CardContent className="px-4 pb-4">
-        <CardTitle>{title}</CardTitle>
+      <CardContent className="px-4 flex-grow">
+        <div className="flex flex-wrap gap-2">
+          {tools.map((tool, index) => (
+            <Badge key={index} variant={"outline"} className="bg-gray-100">
+              {tool}
+            </Badge>
+          ))}
+        </div>
       </CardContent>
-      <CardFooter className="px-4 pb-4 justify-between">
-        <Button asChild>
-          <Link href={website} target="_blank" rel="noopener noreferrer">
-            <RiChromeFill /> Live Website
+
+      <CardFooter className="flex justify-between px-4">
+        <Button size="sm" asChild>
+          <Link href={website}>
+            <RiChromeFill className="size-5" /> Demo
           </Link>
         </Button>
-        {github === "" ? null : (
-          <Button asChild>
-            <Link href={github} target="_blank" rel="noopener noreferrer">
-              <RiGithubFill /> Github
+
+        {github && (
+          <Button variant="outline" size="sm" asChild>
+            <Link href={github}>
+              <RiGithubFill className="size-5" /> Github
             </Link>
           </Button>
         )}
